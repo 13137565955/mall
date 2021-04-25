b81272b7ae4751ef127afc65b10305cea6a77276
13137565955 <1431909656@qq.com>
2021-04-25 22:10:21 +0800

    其他页面的完善

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,146 +1,114 @@
 <template>
- <div>
+ <div id="category">
   <nav-bar class="nav">
     <template v-slot:center><div>分类</div></template>
   </nav-bar>
-  <div class="wrapper">
-    <ul class="content">
-      <li @click="itemclick">分类列表1</li>
-      <li>分类列表2</li>
-      <li>分类列表3</li>
-      <li>分类列表4</li>
-      <li>分类列表5</li>
-      <li>分类列表6</li>
-      <li>分类列表7</li>
-      <li>分类列表8</li>
-      <li>分类列表9</li>
-      <li>分类列表10</li>
-      <li>分类列表11</li>
-      <li>分类列表12</li>
-      <li>分类列表13</li>
-      <li>分类列表14</li>
-      <li>分类列表15</li>
-      <li>分类列表16</li>
-      <li>分类列表17</li>
-      <li>分类列表18</li>
-      <li>分类列表19</li>
-      <li>分类列表20</li>
-      <li>分类列表21</li>
-      <li>分类列表22</li>
-      <li>分类列表23</li>
-      <li>分类列表24</li>
-      <li>分类列表25</li>
-      <li>分类列表26</li>
-      <li>分类列表27</li>
-      <li>分类列表28</li>
-      <li>分类列表29</li>
-      <li>分类列表30</li>
-      <li>分类列表31</li>
-      <li>分类列表32</li>
-      <li>分类列表33</li>
-      <li>分类列表34</li>
-      <li>分类列表35</li>
-      <li>分类列表36</li>
-      <li>分类列表37</li>
-      <li>分类列表38</li>
-      <li>分类列表39</li>
-      <li>分类列表40</li>
-      <li>分类列表41</li>
-      <li>分类列表42</li>
-      <li>分类列表43</li>
-      <li>分类列表44</li>
-      <li>分类列表45</li>
-      <li>分类列表46</li>
-      <li>分类列表47</li>
-      <li>分类列表48</li>
-      <li>分类列表49</li>
-      <li>分类列表50</li>
-      <li>分类列表51</li>
-      <li>分类列表52</li>
-      <li>分类列表53</li>
-      <li>分类列表54</li>
-      <li>分类列表55</li>
-      <li>分类列表56</li>
-      <li>分类列表57</li>
-      <li>分类列表58</li>
-      <li>分类列表59</li>
-      <li>分类列表60</li>
-      <li>分类列表61</li>
-      <li>分类列表62</li>
-      <li>分类列表63</li>
-      <li>分类列表64</li>
-      <li>分类列表65</li>
-      <li>分类列表66</li>
-      <li>分类列表67</li>
-      <li>分类列表68</li>
-      <li>分类列表69</li>
-      <li>分类列表70</li>
-      <li>分类列表71</li>
-      <li>分类列表72</li>
-      <li>分类列表73</li>
-      <li>分类列表74</li>
-      <li>分类列表75</li>
-      <li>分类列表76</li>
-      <li>分类列表77</li>
-      <li>分类列表78</li>
-      <li>分类列表79</li>
-      <li>分类列表80</li>
-      <li>分类列表81</li>
-      <li>分类列表82</li>
-      <li>分类列表83</li>
-      <li>分类列表84</li>
-      <li>分类列表85</li>
-      <li>分类列表86</li>
-      <li>分类列表87</li>
-      <li>分类列表88</li>
-      <li>分类列表89</li>
-      <li>分类列表90</li>
-      <li>分类列表91</li>
-      <li>分类列表92</li>
-      <li>分类列表93</li>
-      <li>分类列表94</li>
-      <li>分类列表95</li>
-      <li>分类列表96</li>
-      <li>分类列表97</li>
-      <li>分类列表98</li>
-      <li>分类列表99</li>
-      <li>分类列表100</li>
-  </ul>
-  </div>
-  
+  <div class="content">
+      <tab-menu :categories="categories"
+                @selectItem="selectItem"></tab-menu>
+      <scroll id="tab-content" :data="[categoryData]">
+        <div>
+          <tab-content-category :subcategories="showSubcategory"></tab-content-category>
+          <option-card  :opationcard="['流行','新款','精选']"
+            @optioncard ="tabClick" ></option-card>
+          <tab-content-detail :category-detail="showCategoryDetail"></tab-content-detail>  
+        </div>
+      </scroll>
+    </div>
   </div>
 </template>
 
 <script>
-import BetterScroll, { PullUpLoad } from 'better-scroll'
+  import NavBar from 'components/common/NavBar'
+  import TabMenu from './childComps/TabMenu'
+  import Scroll from 'components/common/Scroll/Scroll.vue'
 
-import NavBar from 'components/common/NavBar'
+  import TabContentCategory from './childComps/TabContentCategory'
+  import TabContentDetail from './childComps/TabContentDetail'
+  import OptionCard from 'components/content/TabControl/OptionCard.vue'
+  import {getCategory, getSubcategory, getCategoryDetail} from "network/category";
+  import {POP, SELL, NEW} from "@/common/const";
+  import {tabControlMixin} from "@/common/mixin";
   export default {
     name: "Category",
     components:{
-      NavBar
+      NavBar,
+      TabMenu,
+      Scroll,
+      TabContentCategory,
+      TabContentDetail,
+      OptionCard,
     },
-    methods: {
-      itemclick(){
-        console.log("a");
+    mixins: [tabControlMixin],
+    data() {
+		  return {
+		    categories: [],
+        categoryData: {
+        },
+        currentIndex: -1
       }
     },
-    mounted() {    
-    let scroll = new BetterScroll('.wrapper', {
-      click:true,
-      pullUpLoad:true,
-      probeType:3
-      })
-      scroll.on('pullingUp',()=>{
-        console.log("aaa");
-        setTimeout(()=>{
-           scroll.finishPullUp();
-        },2000)
-       
-      })
-      scroll.on('scroll',(position)=>{
-        // console.log(position);
-      })
+    created() {
+		  // 1.请求分类数据
+      this._getCategory()
+    },
+    computed: {
+		  showSubcategory() {
+		    if (this.currentIndex === -1) return {}
+        return this.categoryData[this.currentIndex].subcategories
+      },
+      showCategoryDetail() {
+		    if (this.currentIndex === -1) return []
+		    return this.categoryData[this.currentIndex].categoryDetail[this.currentType]
+      }
+    },
+    methods: {
+		  _getCategory() {
+		    getCategory().then(res => {
+		      // 1.获取分类数据
+		      this.categories = res.data.category.list
+          // 2.初始化每个类别的子数据
+          for (let i = 0; i < this.categories.length; i++) {
+            this.categoryData[i] = {
+              subcategories: {},
+              categoryDetail: {
+                'pop': [],
+                'new': [],
+                'sell': []
+              }
+            }
+          }
+          // 3.请求第一个分类的数据
+          this._getSubcategories(0)
+        })
+      },
+      _getSubcategories(index) {
+        this.currentIndex = index;
+		    const mailKey = this.categories[index].maitKey;
+        getSubcategory(mailKey).then(res => {
+          this.categoryData[index].subcategories = res.data
+          this.categoryData = {...this.categoryData}
+          this._getCategoryDetail(POP)
+          this._getCategoryDetail(SELL)
+          this._getCategoryDetail(NEW)
+        })
+      },
+      _getCategoryDetail(type) {
+		    // 1.获取请求的miniWallkey
+        const miniWallkey = this.categories[this.currentIndex].miniWallkey;
+        // 2.发送请求,传入miniWallkey和type
+		    getCategoryDetail(miniWallkey, type).then(res => {
+		      // 3.将获取的数据保存下来
+		      this.categoryData[this.currentIndex].categoryDetail[type] = res
+          this.categoryData = {...this.categoryData}
+        })
+      },
+      /**
+       * 事件响应相关的方法
+       */
+      selectItem(index) {
+        this._getSubcategories(index)
+      }
     }
   }
 </script>
@@ -150,9 +118,21 @@ import NavBar from 'components/common/NavBar'
   background-color: var(--color-tint);
   color: #fff;
 }
-.wrapper{
-  height: 200px;
-  background-color: red;
-  overflow: hidden;
-}
+ #category {
+    height: 100vh;
+  }
+  .content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 44px;
+    bottom: 49px;
+    overflow: hidden;
+    display: flex;
+  }
+
+  #tab-content {
+    height: 100%;
+    flex: 1;
+  }
 </style>
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,13 +1,36 @@
 <template>
-  <h2>个人</h2>
+  <div id="profile">
+    <nav-bar class="nav-bar">
+      <template v-slot:center><div>Coderwh商城</div></template>
+    </nav-bar>
+
+    <img src="~assets/img/profile/profile.png" class="a"/>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Profile"
-  }
+import NavBar from 'components/common/NavBar';
+	export default {
+		name: "Profile",
+    components: {
+      NavBar
+    },
+	}
 </script>
 
 <style scoped>
+  #profile {
+    background-color: #f2f2f2;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+ .a{
+   width: 100%;
+   height: 100%;
+ }
+  
 
 </style>
